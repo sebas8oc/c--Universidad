53f6f4dfca90bb84b400615d69d476c5f0606af5
sebas8oc <99769172+sebas8oc@users.noreply.github.com>
2026-04-13 21:41:20 -0500

    Implement menu operations in bucleMenuOperaciones.cpp

---
+++ bucleMenuOperaciones.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	int opc;
+	int n1, n2, r;
+	
+	for (int x = 0; x < 3; x++){
+		cout << endl << " Hello world ";
+	}
+	
+	do {
+		cout << endl << " ----- MENU ----- \n 1.Suma \n 2.Restar \n 3.Multiplicar \n 4.Dividir \n 5. Salir";
+		
+		cout << endl << "Digite un una opcion: ";
+		cin >> opc;
+		
+		
+		
+		switch(opc){
+		case 1:
+			cout << "Digite x: ";
+			cin >> n1;
+			
+			cout << "Digite y: ";
+			cin >> n2;
+			
+			r = n1 * n2;
+			cout << endl << r;
+			break;
+		case 2:
+			cout << "Digite x: ";
+			cin >> n1;
+			
+			cout << "Digite y: ";
+			cin >> n2;
+			
+			r = n1 - n2;
+			cout << endl << r;
+			break;
+		case 3:
+			cout << "Digite x: ";
+			cin >> n1;
+			
+			cout << "Digite y: ";
+			cin >> n2;
+			
+			r = n1 * n2;
+			cout << endl << r;
+			break;
+		case 4:
+			cout << "Digite x: ";
+			cin >> n1;
+			
+			cout << "Digite y: ";
+			cin >> n2;
+			
+			if (n2 != 0) {
+				r = n1 / n2;
+				cout << endl << r;
+				
+			} else { 
+				cout << endl << " --------- ERROR --------- ";
+			}
+			break;
+		default: 
+		break;
+		}
+	} while( opc != 5);
+	
+	return 0;
+}
