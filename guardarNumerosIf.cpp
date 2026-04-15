5eeede2fe9c250676e9ed5aacb8bf349a1c9d90d
sebas8oc <99769172+sebas8oc@users.noreply.github.com>
2026-04-14 19:33:19 -0500

    Add program to store even or odd numbers

---
+++ guardarNumerosIf.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	int vf;
+	int par;
+	int n;
+	int i;
+	
+	
+	cout << "Cuantos numeros desea guardar: ";
+	cin >> vf;
+	
+	cout << "Quiere guardar pares o impares? 1 = Pares / 2 = Impares: ";
+	cin >> par;
+	
+	if ( par == 1) {
+		for (i = 1; i <= vf;) {
+			cout << endl << i<<" Digite un numero par\n";
+			cin >> n;
+			if ( n % 2 == 0) {
+				cout << endl << "Guardando... \n";
+				i++;
+			} else {
+				cout << "No es par";
+			}
+		}
+	} else {
+		for (i = 1; i <= vf;) {
+			cout << endl << i<<" Digite un numero par\n";
+			cin >> n;
+			if ( n % 2 == 1) {
+				cout << endl << "Guardando... \n";
+				i++;
+			} else {
+				cout << "No es par";
+			}
+		}
+	}
+	
+	return 0;
+}
