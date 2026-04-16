f08e74535511bd29225b34c585acd18e66c5bf47
sebas8oc <99769172+sebas8oc@users.noreply.github.com>
2026-04-16 17:55:53 -0500

    Refactor main function and improve output formatting

--- dado.cpp
+++ dado.cpp
@@ -2,10 +2,10 @@
 #include <time.h>
 using namespace std;
 
-int main () {
+int main() {
 
   srand(time(NULL));
-  
+
   int vf;
   int opc;
   int bajos;
@@ -17,27 +17,28 @@ int main () {
   int dado;
 
   do {
-    g = 0;
-    p = 0;
+    cout << endl << " Total partidas Ganadas: " << tg;
+    cout << endl << " Total partidas Perdidas: " << tp;
+    cout << endl << " Total partidas Empatadas: " << te;
     cout << endl << "------MENU------\n 1. Jugar 2. Salir\n";
     cout << endl << " Digite opcion: ";
     cin >> opc;
-    
+
     if (opc == 1) {
-      
+
       cout << endl << "Cuantas veces desea jugar: ";
       cin >> vf;
-      
+
       cout << endl << " Desea jugar con bajoss (1-3) o altas (4-6)\n";
       cout << endl << " Digite 1 para bajoss y 2 para altas: ";
       cin >> bajos;
 
       if (bajos == 1) {
         for (int i = 1; i <= vf; i++) {
-          if (p == vf) {
-            tp += 1;
-          }
-          dado = rand()%(6 - 1 + 1) + 1;
+          //          if (p == vf) {
+          //            tp += 1;
+          //         }
+          dado = rand() % (6 - 1 + 1) + 1;
           cout << endl << " El jugador eligio bajos \n";
           cout << endl << " Lanzando dados... \n";
           cout << endl << " Resultado del dado: " << dado << "\n";
@@ -48,40 +49,35 @@ int main () {
             cout << endl << " Gano ronda " << i << "\n";
             g += 1;
           }
-          cout << endl << " Rondas ganadas: " << g << " Rondas perdidas: " << p << "\n";
+          cout << endl
+               << " Rondas ganadas: " << g << " Rondas perdidas: " << p << "\n";
         }
-        if ( vf % 2 == 0) {
-          if ( g == p ) {
+        if (vf % 2 == 0) {
+          if (g == p) {
             te += 1;
             cout << endl << " Resutltado de la partida EMPATE\n";
-            cout << endl << " Total de empates " << te;
-          } else if ( g > p ) {
+          } else if (g > p) {
             tg += 1;
             cout << endl << " Resultado de la partida GANO\n";
-            cout << endl << " Total de Victorias " << tg;
           } else {
             tp += 1;
             cout << endl << " Resultado de la partida PERDIO\n";
-              cout << endl << " Total de perdidas " << tp;
           }
         } else {
-          if ( g > p ) {
+          if (g > p) {
             tg += 1;
             cout << endl << " Resultado de la partida GANO\n";
-            cout << endl << " Total de Victorias " << tg;
           } else {
             tp += 1;
             cout << endl << " Resultado de la partida PERDIO\n";
-              cout << endl << " Total de perdidas " << tp;
           }
-
         }
       } else if (bajos == 2) {
-         for (int i = 1; i <= vf; i++) {
-          if (p == vf) {
-            tp += 1;
-          }
-          dado = rand()%(6 - 1 + 1) + 1;
+        for (int i = 1; i <= vf; i++) {
+          //         if (p == vf) {
+          //            tp += 1;
+          //          }
+          dado = rand() % (6 - 1 + 1) + 1;
           cout << endl << "El jugador eligio bajos \n";
           cout << endl << "Lanzando dados... \n";
           cout << endl << "Resultado del dado: " << dado;
@@ -93,15 +89,33 @@ int main () {
             g += 1;
           }
           cout << endl << "Rondas ganadas: " << g << " Rondas perdidas: " << p;
+          if (vf % 2 == 0) {
+            if (g == p) {
+              te += 1;
+              cout << endl << " Resutltado de la partida EMPATE\n";
+            } else if (g > p) {
+              tg += 1;
+              cout << endl << " Resultado de la partida GANO\n";
+            } else {
+              tp += 1;
+              cout << endl << " Resultado de la partida PERDIO\n";
+            }
+          } else {
+            if (g > p) {
+              tg += 1;
+              cout << endl << " Resultado de la partida GANO\n";
+            } else {
+              tp += 1;
+              cout << endl << " Resultado de la partida PERDIO\n";
+            }
+          }
         }
-      } else if ( bajos != 1 || bajos != 2   ) {
+      } else if (bajos != 1 || bajos != 2) {
         cout << endl << "Opcion invalida ";
       }
-        
     }
 
-  } while(opc != 2);
-
+  } while (opc != 2);
 
   return 0;
 }
