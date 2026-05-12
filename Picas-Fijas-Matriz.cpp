7ef20b93599889c7f3ac1f363d15eca924f38f8e
sebas8oc <99769172+sebas8oc@users.noreply.github.com>
2026-05-11 21:25:04 -0500

    Refactor game logic and add data input menu

--- Picas-Fijas-Matriz.cpp
+++ Picas-Fijas-Matriz.cpp
@@ -1,137 +1,58 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
 
-#define filas 10
-#define cols 7
-#define Tam 4
-#define Tom 2
+#define Tam 3
 
-int main() {
-
-  int opc, ale, u, cf = 0, cp = 0, ci = 0, intento = 0;
-
-  bool repetido, gano = false;
-
-  int adivinar[Tam], jugador[Tam], historial[filas][cols];
-
-  srand(time(NULL));
-
-  for (int i = 0; i < filas; i++) {
-    for (int j = 0; j < cols; j++) {
-      historial[i][j] = 0;
-    }
-  }
-
-  do {
-    cout << endl
-         << "\n ------ MENU ------ \n 1. Jugar 2. Salir \n Digita tu opcion: ";
-
-    cin >> opc;
-
-    switch (opc) {
-    case 1:
-      // Genera el numero aleatorio apenas el jugador elige la opc 1
-
-      for (int i = 0; i < Tam; i++) {
-
-        do {
-          repetido = false;
-
-          ale = rand() % 10;
-
-          for (int j = 0; j < i; j++) {
-
-            if (ale == adivinar[j]) {
-              repetido = true;
-            }
-          }
-        } while (repetido);
-
-        adivinar[i] = ale;
-      }
-
-      // IMPRIME EL NUMERO A ADIVINAR
-      for (int i = 0; i < Tam; i++) {
-        cout << adivinar[i];
-      }
-
-      cout << endl;
-
-      do {
-        cf = 0;
-        cp = 0;
-        // Suma 1 al numero de intentos
-        intento++;
-        // genera los 4n aleatorios sin repetir
-
-        // PIDE EL NUMERO AL USUARIO
-        for (int i = 0; i < Tam; i++) {
-          do {
-            repetido = false;
-
-            cout << endl << "digita un #: ";
-            cin >> u;
-
-            for (int j = 0; j < i; j++) {
-              if (u == jugador[j] || u > 9) {
-                repetido = true;
-              }
-            }
-          } while (repetido);
-
-          historial[ci][i + 1] = u;
-          jugador[i] = u;
-          historial[ci][0] = intento;
-        }
-
-        // CUENTA PICAS Y filas
-
-        for (int i = 0; i < Tam; i++) {
-          if (adivinar[i] == jugador[i]) {
-            cf++;
-            cp--;
-          }
-          for (int j = 0; j < Tam; j++) {
-            if (adivinar[i] == jugador[j]) {
-              cp++;
-            }
-          }
-        }
-        historial[ci][6] = cf;
-        historial[ci][5] = cp;
-        ci++;
-
-        cout << endl << "HISTORIAL";
-        cout << endl << "I\t*\t*\t*\t*\tP\tF";
-        for (int i = 0; i < filas; i++) {
-          cout << endl;
-          for (int j = 0; j < cols; j++) {
-            cout << historial[i][j] << "\t";
-          }
-        }
-
-        // Revisar si gano
-        if (cf == 4) {
-          gano = true;
-          cout << endl << "Felicidades Ganaste";
-        }
-
-      } while (gano == false);
-
-      break;
-
-    case 2:
-
-      cout << endl << "SALIENDO...";
-      break;
-
-    default:
-      cout << endl << "----- ERROR OPCION INVALIDA -----";
-      break;
-    }
-
-  } while (opc != 2);
-
-  return 0;
+int main(int argc, char *argv[]) {
+	
+	bool lleno = false;
+	
+	int array[Tam][Tam];
+	
+	int opc;
+	
+	do {
+		system("cls");
+		cout << endl << " MENU \n 1. Ingresar datos \n 2. Mostrar datos \n 3. Salir \n ingrese una opcion:  ";
+		cin >> opc;
+		
+		switch (opc) {
+		case 1:
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j ++) {
+					cout << "ingrese un dato: ";
+					cin >> array[i][j];
+				}
+			}
+			
+			lleno = true;
+			break;
+			
+		case 2:
+			if (lleno) {
+				cout <<  "C1\tC2\tC3\t";
+				for (int i = 0; i < Tam; i++) {
+					cout << endl;
+					for (int j = 0; j < Tam; j ++) {
+						
+						cout << array[i][j] << "\t";
+					}
+				}
+			} else {
+				cout << endl << "Priemero debe de ingreaser datos";
+			}
+			
+			break;
+			
+		case 3:
+			cout << "SALIENDO ... SALIENDO ... SALIENDO...";
+			break;
+			
+		default: cout << endl << "OPCION INCORRECTA";
+		}
+		system("pause");
+	} while(opc != 3);
+	
+	return 0;
 }
+
